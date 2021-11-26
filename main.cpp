831819192da2c53bf8cbe862fa172caacaf8ec2e
AdamZygmunt <adam.zygmunt006@gmail.com>
2021-11-26 02:10:31 +0100

    poprawa programu i sprawozdania

--- main.cpp
+++ main.cpp
@@ -24,26 +24,12 @@ int main()
 {
     /* zadeklarowanie zmiennych: liczba (w tresci zadania k, ale nazwalem "liczba", aby uniknac pomylek),
        x-liczba elementow ciagu, suma - na biezaco bedzie sumowac wyrazy ciagu od konca, sc-suma wszystkich wyrazów ciagu*/
-    int  liczba, x, suma, sc ;
 
-    // wyswietlenie kominikatu proszacego o podanie dlugosci ciagu - x
-    cout << "podaj jak dlugi chcsz ciag (a1=1, r=1): ";
-
-    // wprowadzenie wartosci x
-    cin >> x;
-
-    // petla uniemozliwiajaca wprowadzenie dlugosci ciagu < 1
-    while( x<1 )
-        {
-            cout << "podano zla wartosc, sprobuj ponownie ";
-            cin >> x;
-        }
+    int  liczba, suma;
+    float sc, x=8;
 
     // wzór na sume wyrazów ciagu
     sc = ((1+x)/2)*x;
-
-    // utworzenie tablicy dynamicznej
-    int *tab= new int[x];
 
     // wyswietlenie zadanego ciagu
     cout<<"to jest twoj ciag"<< endl;
@@ -53,23 +39,14 @@ int main()
             cout<< i << " ";
         }
 
-    // komunikat proszacy o podanie liczby k
+    // komunikat proszacy o podanie liczby
     cout << "\n" << "podaj liczbe : ";
 
     // wprowadzenie liczby
     cin  >> liczba;
 
-    // jezeli dana liczba jest mniejsza od 1, to najkrotszym ciagiem bedzie jednoelementowy ciag a(liczba)=1
-    if(liczba<0) cout << "1"  << endl;
-
-    // jezeli liczba miesci sie w zakresie <0, x-1>, wyswietlenie jednoelementowego ciagu a(n)=liczba+1
-    else if(liczba>=0 && liczba<=x-1)
-        {
-            cout << "\n" << "najkrotszy ciag to :" << liczba+1 << endl; ;
-        }
-
-    // je¿eli liczba jest wieksza od sumy wyrazów zadanego ciagu, wyswietlenie komunikatu o braku rozwiazania
-    else if ( liczba > sc) cout << "Brak podtablic spelniajacych zadane warunki ";
+    // jezeli liczba jest wieksza od sumy wyrazów zadanego ciagu, wyswietlenie komunikatu o braku rozwiazania
+    if ( liczba >= sc) cout << "Brak podtablic spelniajacych zadane warunki ";
 
     // w przeciwnym wypadku
     else
@@ -81,7 +58,8 @@ int main()
             suma = 0;
 
             // dopoki liczba jest wieksza od wartosci "suma", dodawanie do niej kolejnych elementow zbioru (od konca)
-            // oraz wyswietlanie ich w postaci ciagu wyjsciowego
+            // oraz wyswietlanie ich w postaci ciagu wyjsciowego;
+            // jeżeli suma będzie większa od zadanej liczby, zakończ działanie programu
 
                for(j=x; j>0; j--)
               {
@@ -92,9 +70,6 @@ int main()
 
                 if(suma>liczba)break;
                 }
-
-            // kontrolne wyswietlenie sumy ciagu wyjsciowego
-            cout << "\n suma wynosi :" << suma;
         }
     return 0;
 }
